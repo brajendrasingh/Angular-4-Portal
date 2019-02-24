30ec4d936a11f1412c7cfdce1842081b6ebf5bd8
brajendrasingh <brajendrasingh3clogic@gmail.com>
2019-02-24 16:37:32 +0530

    Card view integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AcademicsComponent } from './academics/academics.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -9,7 +10,11 @@ const routes: Routes = [
   },
   {
     path: 'home', component: HomeComponent
+  },
+  {
+    path: 'academics', component: AcademicsComponent
   }
+
 ];
 
 @NgModule({
